7cc8273a326b6c5a367071001c5bece85695ae1c
sekularacognjen20140128 <sekularacognjen@gmail.com>
2019-06-24 16:27:43 +0200

    Refined find functions

--- src/clj/pet_eligentis/routes/home.clj
+++ src/clj/pet_eligentis/routes/home.clj
@@ -5,7 +5,8 @@
    [pet-eligentis.db.core :as db]
    [pet-eligentis.middleware :as middleware]
    [ring.util.http-response :as response]
-   [selmer.parser :refer [render-file]]))
+   [selmer.parser :refer [render-file]]
+   [monger.operators :refer :all]))
 
 (defn home-page [request]
   (layout/render request "home.html"))
@@ -86,41 +87,114 @@
 (defn logout [request]
   (remove-user! request))
 
-(comment (defn find-dog [request]
-           (let [{{wanderlust :wanderlust-potential kid-friendly-mouthiness :kid_friendly-mthnss
-                   exercise-weight-intensity :exercise_need-weight_gain-intnsty sensitivity :sensitivity
-                   dog-frndly-frndly-twrds-strngrs :dog_frnd-frnd_twrds_strngrs being-alone :being-alone drooling :drooling
-                   easy-train-trainability :easy_train-trnblty shedding-easy-grooming :amnt_shedd-easy_groom energy-level :energy_level
-                   intelligence :intelligence size :size novice_owners :novice_owners prey_drive :prey_drive hot-cold :hot-cold} :params} request]
-             (db/find-all (into {} [[:Trainability (read-string easy-train-trainability)]
-                                    [:Amount-Of-Shedding (read-string shedding-easy-grooming)]
-                                    [:Tolerates-Being-Alone (read-string being-alone)]
-                                    [:Easy-To-Train (read-string easy-train-trainability)]
-                                    [:Drooling-Potential (read-string drooling)]
+(defn find-dog [request]
+  (let [{{wanderlust :wanderlust-potential kid-friendly-mouthiness :kid_friendly-mthnss
+          exercise-weight-intensity :exercise_need-weight_gain-intnsty sensitivity :sensitivity
+          dog-frndly-frndly-twrds-strngrs :dog_frnd-frnd_twrds_strngrs being-alone :being-alone drooling :drooling
+          easy-train-trainability :easy_train-trnblty shedding-easy-grooming :amnt_shedd-easy_groom energy-level :energy_level
+          intelligence :intelligence size :size novice_owners :novice_owners prey_drive :prey_drive hot-cold :hot-cold} :params} request]
+    (println (get request :params))
+    (println (db/find-all (into {} [[:Easy-To-Train (cond (= 5 (read-string easy-train-trainability)) {$gte 1 $lte 5}
+                                                          (= 4 (read-string easy-train-trainability)) {$gte 2 $lte 5}
+                                                          (= 3 (read-string easy-train-trainability)) {$gte 3 $lte 5}
+                                                          (= 2 (read-string easy-train-trainability)) {$gte 4 $lte 5}
+                                                          :else 5)]
+                                    [:Amount-Of-Shedding (cond (= 1 (read-string shedding-easy-grooming)) {$gte 1 $lte 5}
+                                                               (= 2 (read-string shedding-easy-grooming)) {$gte 1 $lte 4}
+                                                               (= 3 (read-string shedding-easy-grooming)) {$gte 1 $lte 3}
+                                                               (= 4 (read-string shedding-easy-grooming)) {$gte 1 $lte 2}
+                                                               :else 1)]
+                                    [:Tolerates-Being-Alone (cond (= 5 (read-string being-alone)) {$gte 1 $lte 5}
+                                                                  (= 4 (read-string being-alone)) {$gte 2 $lte 5}
+                                                                  (= 3 (read-string being-alone)) {$gte 3 $lte 5}
+                                                                  (= 2 (read-string being-alone)) {$gte 4 $lte 5}
+                                                                  :else 5)]
+                                    [:Drooling-Potential (cond (= 1 (read-string drooling)) {$gte 1 $lte 5}
+                                                               (= 2 (read-string drooling)) {$gte 1 $lte 4}
+                                                               (= 3 (read-string drooling)) {$gte 1 $lte 3}
+                                                               (= 4 (read-string drooling)) {$gte 1 $lte 2}
+                                                               :else 1)]
                                     [:Size (read-string size)]
-                                    [:Tolerates-Cold-Weather (if (= hot-cold 1) 5)]
-                                    [:Tolerates-Hot-Weather (if (= hot-cold 5) 5)]
-                                    [:Energy-Level (read-string energy_level)]
+                                    [:Tolerates-Cold-Weather (cond (= 5 (read-string hot-cold)) {$gte 1 $lte 5}
+                                                                   (= 4 (read-string hot-cold)) {$gte 2 $lte 5}
+                                                                   (= 3 (read-string hot-cold)) {$gte 3 $lte 5}
+                                                                   (= 2 (read-string hot-cold)) {$gte 4 $lte 5}
+                                                                   :else 5)]
+                                    [:Tolerates-Hot-Weather (cond (= 1 (read-string hot-cold)) {$gte 1 $lte 5}
+                                                                  (= 2 (read-string hot-cold)) {$gte 2 $lte 5}
+                                                                  (= 3 (read-string hot-cold)) {$gte 3 $lte 5}
+                                                                  (= 4 (read-string hot-cold)) {$gte 4 $lte 5}
+                                                                  :else 5)]
+                                    [:Energy-Level (cond (= 1 (read-string energy-level)) {$gte 1 $lte 5}
+                                                         (= 2 (read-string energy-level)) {$gte 2 $lte 5}
+                                                         (= 3 (read-string energy-level)) {$gte 3 $lte 5}
+                                                         (= 4 (read-string energy-level)) {$gte 4 $lte 5}
+                                                         :else 5)]
                                     [:Intelligence (read-string intelligence)]
-                                    [:Prey-Drive (read-string prey_drive)]
-                                    [:Exercise-Needs (read-string exercise-weight-intensity)]
-                                    [:Sensitivity-Level (read-string sensitivity)]
-                                    [:Incredibly-Kid-Friendly-Dogs (read-string kid-friendly-mouthiness)]
-                                    [:Wanderlust-Potential (read-string wanderlust)]]) "dogs"))))
+                                    [:Prey-Drive (cond (= 1 (read-string prey_drive)) {$gte 1 $lte 5}
+                                                       (= 2 (read-string prey_drive)) {$gte 1 $lte 4}
+                                                       (= 3 (read-string prey_drive)) {$gte 1 $lte 3}
+                                                       (= 4 (read-string prey_drive)) {$gte 1 $lte 2}
+                                                       :else 1)]
+                                    [:Exercise-Needs (cond (= 5 (read-string exercise-weight-intensity)) {$gte 1 $lte 5}
+                                                           (= 4 (read-string exercise-weight-intensity)) {$gte 1 $lte 4}
+                                                           (= 3 (read-string exercise-weight-intensity)) {$gte 1 $lte 3}
+                                                           (= 2 (read-string exercise-weight-intensity)) {$gte 1 $lte 2}
+                                                           :else 1)]
+                                    [:Sensitivity-Level (cond (= 1 (read-string sensitivity)) {$gte 1 $lte 5}
+                                                              (= 2 (read-string sensitivity)) {$gte 1 $lte 4}
+                                                              (= 3 (read-string sensitivity)) {$gte 1 $lte 3}
+                                                              (= 4 (read-string sensitivity)) {$gte 1 $lte 2}
+                                                              :else 1)]
+                                    [:Incredibly-Kid-Friendly-Dogs (cond (= 1 (read-string kid-friendly-mouthiness)) {$gte 1 $lte 5}
+                                                                         (= 2 (read-string kid-friendly-mouthiness)) {$gte 2 $lte 5}
+                                                                         (= 3 (read-string kid-friendly-mouthiness)) {$gte 3 $lte 5}
+                                                                         (= 4 (read-string kid-friendly-mouthiness)) {$gte 4 $lte 5}
+                                                                         :else 5)]
+                                    [:Wanderlust-Potential (cond (= 1 (read-string wanderlust)) {$gte 1 $lte 5}
+                                                                 (= 2 (read-string wanderlust)) {$gte 1 $lte 4}
+                                                                 (= 3 (read-string wanderlust)) {$gte 1 $lte 3}
+                                                                 (= 4 (read-string wanderlust)) {$gte 1 $lte 2}
+                                                                 :else 1)]]) "dogs"))))
 
 (defn find-cat [request]
   (let [{{shedding :shedding tendency-vocalize :tendency_vocalize affec-family :affec_family
           intelligence :intelligence poten-playful :poten_playful pet-frndly :pet_frndly
           kid-frndly :kid_frndly frndly-twrds-strngrs :frndly_twrds_strngrs} :params} request]
-    (println (get request :params))
-    (println (db/find-all (into {} [[:Affectionate-with-Family (read-string affec-family)]
-                                    [:Amount-of-Shedding (read-string shedding)]
-                                    [:Friendly-Toward-Strangers (read-string frndly-twrds-strngrs)]
-                                    [:Intelligence (read-string intelligence)]
-                                    [:Kid-Friendly (read-string kid-frndly)]
-                                    [:Pet-Friendly (read-string pet-frndly)]
+    (println (db/find-all (into {} [[:Affectionate-with-Family (cond (= 5 (read-string affec-family)) {$gte 4 $lte 5}
+                                                                     (= 4 (read-string affec-family)) {$gte 4 $lte 5}
+                                                                     (= 3 (read-string affec-family)) {$gte 3 $lte 4}
+                                                                     :else {$gte 1 $lte 2})]
+                                    [:Amount-of-Shedding (cond (= 1 (read-string shedding)) {$gte 1 $lte 5}
+                                                               (= 2 (read-string shedding)) {$gte 1 $lte 4}
+                                                               (= 3 (read-string shedding)) {$gte 1 $lte 3}
+                                                               (= 4 (read-string shedding)) {$gte 1 $lte 2}
+                                                               :else 1)]
+                                    [:Friendly-Toward-Strangers (cond (= 1 (read-string frndly-twrds-strngrs)) {$gte 1 $lte 5}
+                                                                      (= 2 (read-string frndly-twrds-strngrs)) {$gte 2 $lte 5}
+                                                                      (= 3 (read-string frndly-twrds-strngrs)) {$gte 3 $lte 5}
+                                                                      (= 4 (read-string frndly-twrds-strngrs)) {$gte 4 $lte 5}
+                                                                      :else 5)]
+                                    [:Intelligence (cond (= 5 (read-string intelligence)) {$gte 1 $lte 5}
+                                                         (= 4 (read-string intelligence)) {$gte 1 $lte 4}
+                                                         :else {$gte 1 $lte 3})]
+                                    [:Kid-Friendly (cond (= 1 (read-string kid-frndly)) {$gte 1 $lte 5}
+                                                         (= 2 (read-string kid-frndly)) {$gte 2 $lte 5}
+                                                         (= 3 (read-string kid-frndly)) {$gte 3 $lte 5}
+                                                         (= 4 (read-string kid-frndly)) {$gte 4 $lte 5}
+                                                         :else 5)]
+                                    [:Pet-Friendly (cond (= 1 (read-string pet-frndly)) {$gte 1 $lte 5}
+                                                         (= 2 (read-string pet-frndly)) {$gte 2 $lte 5}
+                                                         (= 3 (read-string pet-frndly)) {$gte 3 $lte 5}
+                                                         (= 4 (read-string pet-frndly)) {$gte 4 $lte 5}
+                                                         :else 5)]
                                     [:Potential-for-Playfulness (read-string poten-playful)]
-                                    [:Tendency-to-Vocalize (read-string tendency-vocalize)]]) "cats"))))
+                                    [:Tendency-to-Vocalize (cond (= 5 (read-string tendency-vocalize)) {$gte 1 $lte 5}
+                                                                 (= 4 (read-string tendency-vocalize)) {$gte 1 $lte 4}
+                                                                 (= 3 (read-string tendency-vocalize)) {$gte 1 $lte 3}
+                                                                 (= 2 (read-string tendency-vocalize)) {$gte 1 $lte 2}
+                                                                 :else 1)]
+                                    ]) "cats"))))
 
 (defn pet-pref-dog-page [request]
   (layout/render request "pet-preferences-dog.html"))
@@ -144,7 +218,8 @@
    ["/pet/yourpet" {:post new-pet-page}]
    ["/pet-preferences-dog" {:get pet-pref-dog-page}]
    ["/pet-preferences-cat" {:get pet-pref-cat-page}]
-   ["/prefered-cat" {:post find-cat}]])
-   ;["/prefered-dog" {:post find-dog}]
-   
+   ["/prefered-cat" {:post find-cat}]
+   ["/prefered-dog" {:post find-dog}]])
+
+
 
